0f66c4be9d166ac7f6903943647812ff25474322
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-22 11:31:17 -0700

    add Vendor's first pass

---
+++ Vendor-and-Order-Tracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests 
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test","test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
