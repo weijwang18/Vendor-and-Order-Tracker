5a55eb796f84c2852d47d277096c6fc82d18c377
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-22 16:36:10 -0700

    add Order's sixth pass

--- Vendor-and-Order-Tracker.Tests/ModelTests/OrderTests.cs
+++ Vendor-and-Order-Tracker.Tests/ModelTests/OrderTests.cs
@@ -73,5 +73,22 @@ namespace VendorAndOrderTracker.Tests
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string title1 = "Tom's order";
+      string description1 = "three croissants";
+      int price1 = 10;
+      DateTime date1 = new DateTime (2022, 7, 22, 7,00,0);
+      Order newOrder1 = new Order(title1, description1, price1, date1);
+      string title2 = "Sam's order";
+      string description2 = "four croissants";
+      int price2 = 15;
+      DateTime date2 = new DateTime (2022, 7, 23, 8,00,0);
+      Order newOrder2 = new Order(title2, description2, price2, date2);
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
+    }
 }
 }
