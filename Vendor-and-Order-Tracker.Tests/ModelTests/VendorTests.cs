bf533fae5186078997a7e85729f40b163dae5cd9
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-22 12:26:32 -0700

    update first test and add Vendor's second pass

--- Vendor-and-Order-Tracker.Tests/ModelTests/VendorTests.cs
+++ Vendor-and-Order-Tracker.Tests/ModelTests/VendorTests.cs
@@ -8,11 +8,24 @@ namespace VendorAndOrderTracker.Tests
   [TestClass]
   public class VendorTests 
   {
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test","test");
+      string name = "Suzie's Cafe";
+      string description = "Specializes in coffee, drinks, and Asian pastries";
+      Vendor newVendor = new Vendor(name, description);
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Suzie's Cafe";
+      string description = "Specializes in coffee, drinks, and Asian pastries";
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
     }
   }
 }
