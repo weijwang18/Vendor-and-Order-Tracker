f7d0fa5d7eab571742677faac9b67d909e0e63ef
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-24 02:52:24 -0700

    WIP: Updating Controllers

---
+++ Vendor-and-Order-Tracker/Models/Order.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System;
+
+namespace VendorAndOrderTracker
+{
+  public class Order
+  {
+    private static List<Order> _instances = new List<Order> {};
+    public string Description { get; set; }
+    public string Title { get; set; }
+    public int Price { get; set; }
+    public DateTime Date { get;}
+    public int Id { get; }
+
+    public Order(string title, string description, int price, DateTime date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
--- Vendor-and-Order-Tracker/Models/Vendor.cs
+++ Vendor-and-Order-Tracker/Models/Vendor.cs
@@ -1,9 +1,42 @@
 using System.Collections.Generic;
 
-namespace VendorAndOrderTracker
+namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
-    // properties, constructors, methods, etc. go here
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
